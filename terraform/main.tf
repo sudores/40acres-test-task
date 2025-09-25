98002e5f97054fa681b715ef894dfa7eec7664bb
Horobchenko Andrii <andrii.horobchenko>
2025-09-25 23:43:13 +0300

    Fixed argocd and bitcoind

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,13 +8,35 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
+  version    = "8.5.6"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
 
-  values = [ ]
+  values = [
+    yamlencode({
+      server = {
+        ingress = {
+          enabled          = true
+          ingressClassName = "nginx"
+          hostname         = "argo.gzttk.loc"
+          annotations = {
+            "kubernetes.io/ingress.class" = "nginx"
+          }
+          tls       = [] # For Minikube testing, disable TLS
+          extraArgs = ["--insecure"]
+          params = {
+            "server.insecure" = true
+          }
+        }
+        service = {
+          type = "ClusterIP" # optional; ingress will handle exposure
+        }
+      }
+    })
+  ]
 
   depends_on = [kubernetes_namespace.argocd]
   provisioner "local-exec" {
-      command = "kubectl apply -n argocd -f ./assets/argocd-root-app.yaml"
+    command = "kubectl apply -n argocd -f ./assets/argocd-root-app.yaml"
   }
 }
 
